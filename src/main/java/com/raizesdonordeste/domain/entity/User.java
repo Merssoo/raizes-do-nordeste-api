85db7f59cf29a3caaf4e34311d28ab58dc65a116
Merssoo <emerson.cardoso.trindade@gmail.com>
2026-06-17 22:06:22 -0300

    #MVP SPRINT01: Ajuste

---
+++ src/main/java/com/raizesdonordeste/domain/entity/User.java
@@ -0,0 +1,31 @@
+package com.raizesdonordeste.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "seq_user", allocationSize = 1)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    private Boolean ativo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
