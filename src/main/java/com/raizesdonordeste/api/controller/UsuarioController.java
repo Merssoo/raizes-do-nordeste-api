4ba457b9e37f430dc5fe29bea4fd39c3b88d44a3
Merssoo <emerson.cardoso.trindade@gmail.com>
2026-06-20 21:43:57 -0300

    #MVP FIX: Ajustes remoção da api criada incorretamnete

--- src/main/java/com/raizesdonordeste/api/controller/UsuarioController.java
+++ src/main/java/com/raizesdonordeste/api/controller/UsuarioController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/usuarios")
 @RequiredArgsConstructor
 public class UsuarioController {
 
